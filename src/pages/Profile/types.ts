1b64fc82412fc2291a97c03153f21b806cc77a57
ruslan <rnagumanov57@gmail.com>
2023-12-22 19:13:50 +0300

    [add-avatar-images]

--- src/pages/Profile/types.ts
+++ src/pages/Profile/types.ts
@@ -3,6 +3,7 @@ export interface IProfile {
     [PROFILE_KEYS.PASSWORD]: string;
     [PROFILE_KEYS.FIRST_NAME]: string;
     [PROFILE_KEYS.LAST_NAME]: string;
+    [PROFILE_KEYS.URL]: string;
     id: number;
 }
 
@@ -11,10 +12,18 @@ export enum PROFILE_KEYS {
     PASSWORD = 'password',
     FIRST_NAME = 'firstName',
     LAST_NAME = 'lastName',
+    URL = 'url',
 }
 
 export interface UserProfileProps {
     id: number;
 }
 
-export const initState = { [PROFILE_KEYS.EMAIL]: '', [PROFILE_KEYS.PASSWORD]: '', [PROFILE_KEYS.FIRST_NAME]: '', [PROFILE_KEYS.LAST_NAME]: '', id: 0 };
+export const initState = {
+    [PROFILE_KEYS.EMAIL]: '',
+    [PROFILE_KEYS.PASSWORD]: '',
+    [PROFILE_KEYS.LAST_NAME]: '',
+    [PROFILE_KEYS.FIRST_NAME]: '',
+    [PROFILE_KEYS.URL]: '',
+    id: 0,
+};

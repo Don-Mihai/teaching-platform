42b2703d59966d1f2f3c9434b729e6a25326788b
ruslan <rnagumanov57@gmail.com>
2023-12-25 20:49:36 +0300

    [add-change-header-groups]

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -5,3 +5,12 @@ export enum PAGE_ROUTES {
     Tasks = '/tasks',
     Profile = '/profile',
 }
+
+export interface IGroups {
+    group: string;
+}
+
+export interface IGroup {
+    teacher: string;
+    students: string;
+}

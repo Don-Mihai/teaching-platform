92dc6ed402a375050063041fecc1887250cf8a53
AlexeyOkhotnikov <alexeyps84@gmail.com>
2024-01-21 13:09:50 +0300

    todo

--- src/pages/Profile/UserData/Inputs/Index.tsx
+++ src/pages/Profile/UserData/Inputs/Index.tsx
@@ -1,6 +1,6 @@
 import { Button, TextField } from '@mui/material';
 import { ChangeEvent, FC, FormEvent } from 'react';
-import { IProfile, PROFILE_KEYS } from '../types';
+import { IProfile, PROFILE_KEYS } from '../../types';
 import './Inputs.scss';
 
 interface AddUserProps {
@@ -11,7 +11,7 @@ interface AddUserProps {
 
 const Inputs: FC<AddUserProps> = ({ formValues, handleChange, handleSubmit }) => {
     return (
-        <div className="right-component">
+        <>
             <div className="profile-component__input-email">
                 <TextField
                     name="email"
@@ -35,29 +35,17 @@ const Inputs: FC<AddUserProps> = ({ formValues, handleChange, handleSubmit }) =>
                 />
             </div>
             <div className="profile-component__input-first-name">
-                <TextField
-                    name="firstName"
-                    type="text"
-                    value={formValues?.[PROFILE_KEYS.FIRST_NAME]}
-                    onChange={handleChange}
-                    label={'First Name'}
-                ></TextField>
+                <TextField name="firstName" type="text" value={formValues?.[PROFILE_KEYS.FIRST_NAME]} onChange={handleChange} label={'First Name'}></TextField>
             </div>
             <div className="profile-component__input-last-name">
-                <TextField
-                    name="lastName"
-                    type="text"
-                    value={formValues?.[PROFILE_KEYS.LAST_NAME]}
-                    onChange={handleChange}
-                    label={'Last Name'}
-                ></TextField>
+                <TextField name="lastName" type="text" value={formValues?.[PROFILE_KEYS.LAST_NAME]} onChange={handleChange} label={'Last Name'}></TextField>
             </div>
             <div className="profile-component__button-save">
                 <Button onClick={handleSubmit} size="large" sx={{ textTransform: 'none' }} variant="contained">
                     Save changes
                 </Button>
             </div>
-        </div>
+        </>
     );
 };
 
--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,65 +1,28 @@
-import Header from '../../components/Header';
 import './Profile.scss';
-import Button from '@mui/material/Button';
-import { pink } from '@mui/material/colors';
-import Inputs from './Inputs/Index';
-import { ChangeEvent, FormEvent, useEffect, useState } from 'react';
-import { IProfile, PROFILE_KEYS, initState } from './types';
-import axios from 'axios';
-
-import { Avatar } from '@mui/material';
-import FileDrop from '../../components/FileDrop';
+import Header from '../../components/Header';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../redux/store';
-import Level from '../../components/Level';
+import UserData from './UserData';
+import Tasks from './UserData/Tasks';
 
 const Profile = () => {
-    const [formValues, setFormValues] = useState<Partial<IProfile>>(initState);
-    const lessons = useSelector((store: RootState) => store.lesson.lessons);
     const user = useSelector((store: RootState) => store.user.user);
 
-    useEffect(() => {
-        fetchUser();
-    }, []);
-
-    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-        const { name, value } = event.target;
-        setFormValues({ ...formValues, [name]: value });
-    };
-
-    const handleSubmit = async (event: FormEvent) => {
-        await axios.put(`users/${formValues.id}`, formValues);
-    };
-
-    const fetchUser = async () => {
-        setFormValues(user);
-    };
+    const tasks = 'Задания'
+    const todo = '12'
+    const inProgress = '1'
+    const complete = '3'
 
     return (
         <div className="profile-page">
             <Header />
             <div className="profile-page__content">
-                <div className="profile-page__user-data">
-                    <div className="profile-page__left">
-                        <div className="profile-page__avatar-container">
-                            <FileDrop onSendFiles={() => { }}>
-                                <Avatar className='profile-page__avatar' src={formValues?.[PROFILE_KEYS.URL]}>
-                                    {`${formValues?.[PROFILE_KEYS.FIRST_NAME]?.charAt(0) || ''}${formValues?.[PROFILE_KEYS.LAST_NAME]?.charAt(0) || ''}`}
-                                </Avatar>
-                            </FileDrop>
-                            <div className="profile-page__title-wrap">
-                                <h3 className="profile-page__avatar-title">{user.firstName}</h3>
-                                <h4 className="profile-page__avatar-date">Join 12.13.2024</h4>
-                            </div>
-                        </div>
-                        <Level level={1} xp={250} maxXp={500} />
-                    </div>
-                    <div className="profile-page__right">
-                        <Inputs handleChange={handleChange} handleSubmit={handleSubmit} formValues={formValues} />
-                    </div>
+                <UserData user={user} />
+                <div className='tasks'>
+                    <Tasks title={tasks} children={`${todo}/${inProgress}/${complete}`} />
                 </div>
-
             </div>
+
         </div >
     );
 };

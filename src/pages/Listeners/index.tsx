7d4675af82acb7257e41a1da07f7aa40c0282665
mihai807 <mihai807@mail.ru>
2024-01-27 12:37:48 +0300

    fix

--- src/pages/Listeners/index.tsx
+++ src/pages/Listeners/index.tsx
@@ -6,7 +6,7 @@ import { useEffect, useState } from 'react';
 import { getListeners } from '../../redux/User';
 import TableSortLabel from '@mui/material/TableSortLabel';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
-import { MenuItem } from '@mui/material';
+import { InputLabel, MenuItem } from '@mui/material';
 import { Avatar, Box, FormControl, TextField } from '@mui/material';
 import { AccountCircle } from '@mui/icons-material';
 
@@ -14,7 +14,7 @@ const Listeners = () => {
     const listeners = useSelector((store: RootState) => store.user.listeners);
     const dispatch = useDispatch<AppDispatch>();
 
-    const [item, setItem] = useState('');
+    const [item, setItem] = useState('1');
 
     const handleChange = (event: SelectChangeEvent) => {
         setItem(event.target.value);
@@ -34,11 +34,21 @@ const Listeners = () => {
             <div className="listeners-page__content">
                 <div className="listeners-page__lists">
                     <h1 className="listeners-page__head-title">Слушатели</h1>
-                    <Select labelId="demo-simple-select-filled-label" id="demo-simple-select-filled" value={item} onChange={handleChange} label="Sort">
-                        {listeners.map(item => {
-                            return <MenuItem value={item.firstName}>{item.firstName}</MenuItem>;
-                        })}
-                    </Select>
+                    <div>
+                        <Select
+                            sx={{ width: '200px' }}
+                            labelId="demo-simple-select-filled-label"
+                            id="demo-simple-select-filled"
+                            value={item}
+                            onChange={handleChange}
+                            label="Sort"
+                        >
+                            <MenuItem value={1}>По алфавиту</MenuItem>
+                            <MenuItem value={2}>По времени добавления</MenuItem>
+                            <MenuItem value={3}>По уровню</MenuItem>;
+                        </Select>
+                    </div>
+
                     <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
                         <AccountCircle sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
                         <TextField fullWidth id="input-with-sx" label="Поиск" variant="standard" />

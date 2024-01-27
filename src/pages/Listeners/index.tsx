6c19f07751d9bb593100bf7bb33f06977ccdba59
Ruslan <rnagumanov57@gmail.com>
2024-01-27 12:21:19 +0300

    add-components

--- src/pages/Listeners/index.tsx
+++ src/pages/Listeners/index.tsx
@@ -4,6 +4,8 @@ import './Listeners.scss';
 import { AppDispatch, RootState } from '../../redux/store';
 import { useEffect } from 'react';
 import { getListeners } from '../../redux/User';
+import { Avatar, Box, FormControl, TextField } from '@mui/material';
+import { AccountCircle } from '@mui/icons-material';
 
 const Listeners = () => {
     const listeners = useSelector((store: RootState) => store.user.listeners);
@@ -21,10 +23,21 @@ const Listeners = () => {
         <div className="listeners-page">
             <Header />
             <div className="listeners-page__content">
-                <h1>Your Listeners</h1>
-                {listeners.map(item => {
-                    return <div>{item.firstName}</div>;
-                })}
+                <div className="listeners-page__lists">
+                    <h1 className="listeners-page__head-title">Слушатели</h1>
+                    <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+                        <AccountCircle sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+                        <TextField fullWidth id="input-with-sx" label="Поиск" variant="standard" />
+                    </Box>
+                    {listeners.map(item => {
+                        return (
+                            <div className="listeners-page__list">
+                                <Avatar src="" sx={{ width: '50px', height: '50px', marginRight: '10px' }} />
+                                <h2 className="listeners-page__title">{item.firstName}</h2>
+                            </div>
+                        );
+                    })}
+                </div>
             </div>
         </div>
     );

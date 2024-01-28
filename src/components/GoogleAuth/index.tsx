420269bcd96f1a2e7101845d529c251ecda71d7b
mihai807 <mihai807@mail.ru>
2024-01-28 13:26:47 +0300

    api

--- src/components/GoogleAuth/index.tsx
+++ src/components/GoogleAuth/index.tsx
@@ -1,11 +1,28 @@
 import { GoogleLogin } from '@react-oauth/google';
 
 const GoogleAuth = () => {
+    const handleSuccess = async (credentialResponse: any) => {
+        const token = credentialResponse.credential;
+
+        const response = await fetch(`https://www.googleapis.com/oauth2/v3/userinfo`, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+                Accept: 'application/json',
+            },
+        });
+
+        if (response.ok) {
+            const userData = await response.json();
+            console.log('User Data:', userData);
+            // Здесь вы получите данные пользователя, такие как имя и фамилия
+        } else {
+            console.log('Failed to fetch user data');
+        }
+    };
+
     return (
         <GoogleLogin
-            onSuccess={credentialResponse => {
-                console.log(credentialResponse);
-            }}
+            onSuccess={handleSuccess}
             onError={() => {
                 console.log('Login Failed');
             }}

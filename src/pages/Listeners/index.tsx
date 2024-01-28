ff64fcb698eaa2fe567511160317e3d84be7f2c5
ruslan <rnagumanov57@gmail.com>
2024-01-28 12:12:37 +0300

    add-sort

--- src/pages/Listeners/index.tsx
+++ src/pages/Listeners/index.tsx
@@ -4,10 +4,9 @@ import './Listeners.scss';
 import { AppDispatch, RootState } from '../../redux/store';
 import { useEffect, useState } from 'react';
 import { getListeners } from '../../redux/User';
-import TableSortLabel from '@mui/material/TableSortLabel';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 import { InputLabel, MenuItem } from '@mui/material';
-import { Avatar, Box, FormControl, TextField } from '@mui/material';
+import { Avatar, Box, TextField } from '@mui/material';
 import { AccountCircle } from '@mui/icons-material';
 
 const Listeners = () => {
@@ -15,10 +14,7 @@ const Listeners = () => {
     const dispatch = useDispatch<AppDispatch>();
 
     const [item, setItem] = useState('1');
-
-    const handleChange = (event: SelectChangeEvent) => {
-        setItem(event.target.value);
-    };
+    const [searchValue, setSearchValue] = useState('');
 
     useEffect(() => {
         fetchListeners();
@@ -27,6 +23,26 @@ const Listeners = () => {
     const fetchListeners = async () => {
         dispatch(getListeners());
     };
+
+    const handleChange = (event: SelectChangeEvent) => {
+        setItem(event.target.value);
+    };
+
+    const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchValue(event.target.value);
+    };
+
+    const filteredListeners = listeners
+        .filter(item => {
+            const fullName = `${item.firstName} ${item.lastName || ''}`.toLowerCase();
+            return fullName.includes(searchValue.toLowerCase());
+        })
+        .sort((a, b) => {
+            if (a.firstName === b.firstName) {
+                return (a.lastName || '').localeCompare(b.lastName || '');
+            }
+            return (a.firstName || '').localeCompare(b.firstName || '');
+        });
 
     return (
         <div className="listeners-page">
@@ -45,19 +61,23 @@ const Listeners = () => {
                         >
                             <MenuItem value={1}>По алфавиту</MenuItem>
                             <MenuItem value={2}>По времени добавления</MenuItem>
-                            <MenuItem value={3}>По уровню</MenuItem>;
+                            <MenuItem value={3}>По уровню</MenuItem>
                         </Select>
                     </div>
 
                     <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
                         <AccountCircle sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
-                        <TextField fullWidth id="input-with-sx" label="Поиск" variant="standard" />
+                        <TextField fullWidth id="input-with-sx" label="Поиск" variant="standard" value={searchValue} onChange={handleSearchChange} />
                     </Box>
-                    {listeners.map(item => {
+                    {filteredListeners.map(item => {
                         return (
                             <div className="listeners-page__list">
-                                <Avatar src="" sx={{ width: '50px', height: '50px', marginRight: '10px' }} />
-                                <h2 className="listeners-page__title">{item.firstName}</h2>
+                                <Avatar src={item.url} className="listeners-page__avatar">
+                                    {`${item.firstName?.charAt(0) || ''}${item.lastName?.charAt(0) || ''}`}
+                                </Avatar>
+                                <h2 className="listeners-page__title">
+                                    {item.firstName} {item.lastName}
+                                </h2>
                             </div>
                         );
                     })}

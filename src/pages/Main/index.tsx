9f8407d70e201e430480554d41f408cf83f3a8b5
mihai807 <mihai807@mail.ru>
2024-03-02 11:25:35 +0300

    fix

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,12 +1,17 @@
 import GoogleAuth from '../../components/GoogleAuth';
 import Group from '../../components/Group';
+import { IGroup } from '../../redux/Group/types';
 import './Main.scss';
 
 const Main = () => {
-    const groups: any[] = [
+    const groups: IGroup[] = [
         {
+            id: 1,
             name: 'ВТ/ЧТ',
-            url: 'https://jazz.sber.ru/zthtbf?psw=OBEDV0APXRVZAQRBHxtKBlYLGQ',
+            url: {
+                sber: 'https://jazz.sber.ru/zthtbf?psw=OBEDV0APXRVZAQRBHxtKBlYLGQ',
+                inordic: 'http://study.inordic.ru/panel/users_lesson_visits?group=262',
+            },
         },
     ];
 
